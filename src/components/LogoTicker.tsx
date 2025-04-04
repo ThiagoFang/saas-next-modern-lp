b8feff02b024d3c8364dd15a1817f73584aa1fd5
ThiagoFang <thiagofang@gmail.com>
2025-04-04 14:39:42 -0300

    logo tickers section

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -4,16 +4,35 @@ import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
+	{ src: acmeLogo, alt: "Acme Logo" },
+	{ src: quantumLogo, alt: "Quantum Logo" },
+	{ src: echoLogo, alt: "Echo Logo" },
+	{ src: celestialLogo, alt: "Celestial Logo" },
+	{ src: pulseLogo, alt: "Pulse Logo" },
+	{ src: apexLogo, alt: "Apex Logo" },
 ];
 
 export const LogoTicker = () => {
-  return null;
+	return (
+		<section className="bg-black text-white py-18 sm:py-24">
+			<div className="container">
+				<h2 className="text-xl text-center text-white">
+					Trusted by the world's most innovative teams
+				</h2>
+				<div className="flex justify-center gap-16 mt-9 overflow-hidden before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,rgb(0,0,0),rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,rgb(0,0,0),rgb(0,0,0,0))]">
+					{images.map(({ alt, src }) => (
+						<Image
+							src={src}
+							alt={alt}
+							key={alt}
+							className="flex-none h-8 w-auto"
+						/>
+					))}
+				</div>
+			</div>
+		</section>
+	);
 };
