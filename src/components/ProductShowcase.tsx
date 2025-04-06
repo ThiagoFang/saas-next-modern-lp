3f97181328f5d6b36d7ab3d87dd3e58b62f18ea0
ThiagoFang <thiagofang@gmail.com>
2025-04-06 16:11:06 -0300

    product showcase scroll effect

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "motion/react";
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+	const ref = useRef<HTMLImageElement>(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start end", "end end"],
+	});
+
+	const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+	const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
 	return (
-		<div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-18 sm:py-24">
+		<div className="bg-black overflow-x-clip text-white bg-gradient-to-b from-black to-[#5D2CA8] py-18 sm:py-24">
 			<div className="container">
 				<h2 className="tracking-tight text-center text-5xl sm:text-6xl font-bold">
 					Intuitive interface
@@ -15,11 +29,20 @@ export const ProductShowcase = () => {
 						task at a time
 					</p>
 				</div>
-				<Image
-					src={appScreen}
-					alt="The product screenshot"
-					className="mt-14 mx-auto"
-				/>
+				<motion.div
+					style={{
+						opacity,
+						rotateX,
+						transformPerspective: "800px",
+					}}
+				>
+					<Image
+						src={appScreen}
+						alt="The product screenshot"
+						className="mt-14 mx-auto"
+						ref={ref}
+					/>
+				</motion.div>
 			</div>
 		</div>
 	);
